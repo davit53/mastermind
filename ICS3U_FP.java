ed77ee5afdb4e2227b8210a0625efcaed35e570e
Davit Najaryan <najaryan.d@gmail.com>
2022-06-08 21:21:43 -0400

    Semi-Working Version

--- ICS3U_FP.java
+++ ICS3U_FP.java
@@ -17,25 +17,9 @@ public class ICS3U_FP {
         Random random = new Random();
         String[] code = new String[4];
         String[] codeBreaker = new String[4];
-        int i = 0, j = 0, k = 0, temp = 0;
+        int i = 0, j = 0, k = 0, temp = 0, counterForRed = 0, numberOfTries = 0;
         Boolean verifyAnswer = false;
 
-        //User Input
-        for(j = 0; j < 4; j++){
-            System.out.println("Please enter colour of the code (only 1 letter): ");
-            codeBreaker[j] = input.next();
-            while(codeBreaker[j].length() != 1){
-                System.out.println("----Please enter only 1 letter----");
-                codeBreaker[j] = input.next();
-            }
-        }
-
-        //Make Capital
-        System.out.println("Your Guess: ");
-        for(j = 0; j < 4; j++){
-            codeBreaker[j] = codeBreaker[j].toUpperCase();
-            System.out.print(codeBreaker[j] + " ");
-        }
 
         System.out.println("\nThe code is: ");
 
@@ -68,49 +52,81 @@ public class ICS3U_FP {
         } while(code[0].equals(code[i]) || code[1].equals(code[i]) || code[2].equals(code[i]));
         System.out.print(code[i] + " ");
 
-
-        /*
-        Check if correct
-        */
-        //Case if all mathes
-        for(j = 0; j < 4; j++){
-            if(codeBreaker[j].equals(code[j])){
-                verifyAnswer = true;
+        do{
+            //Print based on correct or incorrect code
+            if(verifyAnswer == true){
+                System.out.println("\nThat is the correct code!");
             }
             else{
-                verifyAnswer = false;
-                break;
+                //User Input
+                for(j = 0; j < 4; j++){
+                    System.out.println("Please enter colour of the code (only 1 letter): ");
+                    codeBreaker[j] = input.next();
+                    while(codeBreaker[j].length() != 1){
+                        System.out.println("----Please enter only 1 letter----");
+                        codeBreaker[j] = input.next();
+                    }
+                }
+                //Make Capital
+                System.out.println("Your Guess: ");
+                for(j = 0; j < 4; j++){
+                    codeBreaker[j] = codeBreaker[j].toUpperCase();
+                    System.out.print(codeBreaker[j] + " ");
+                }
+                /*
+                Check if correct
+                */
+                //Case if all mathes
+                for(j = 0; j < 4; j++){
+                    if(codeBreaker[j].equals(code[j])){
+                        verifyAnswer = true;
+                    }
+                    else{
+                        verifyAnswer = false;
+                        break;
+                    }
+                }
+
+                //Case for Red Pugs
+                System.out.println("\n\n");
+                for(i = 0; i < 4; i ++){
+                    if(counterForRed == 0){
+                        for(counterForRed = 0; counterForRed < 4; counterForRed++){
+                            if(codeBreaker[counterForRed].equals(code[counterForRed])){
+                                System.out.println("Red Pug");
+                            }
+                        }
+                    }
+                }
+                //Case for White Pugs
+                if(codeBreaker[0].equals(code[0]) == false && 
+                (codeBreaker[0].equals(code[1]) 
+                || codeBreaker[0].equals(code[2])
+                || codeBreaker[0].equals(code[3]))){
+                    System.out.println("White Pug");
+                }
+                if(codeBreaker[1].equals(code[1]) == false && 
+                (codeBreaker[1].equals(code[0]) 
+                || codeBreaker[1].equals(code[2])
+                || codeBreaker[1].equals(code[3]))){
+                    System.out.println("White Pug");
+                }
+                if(codeBreaker[2].equals(code[2]) == false && 
+                (codeBreaker[2].equals(code[0]) 
+                || codeBreaker[2].equals(code[1])
+                || codeBreaker[2].equals(code[3]))){
+                    System.out.println("White Pug");
+                }
+                if(codeBreaker[3].equals(code[3]) == false && 
+                (codeBreaker[3].equals(code[0]) 
+                || codeBreaker[3].equals(code[1])
+                || codeBreaker[3].equals(code[2]))){
+                    System.out.println("White Pug");
+                }
             }
-        }
 
-        System.out.println("\n\n");
-        for(temp = 0; temp < 4; temp ++){
-            if(codeBreaker[temp].equals(code[k])){
-                System.out.println("Red pug");
-            }
-            else if(codeBreaker[temp].equals(code[k+1]) 
-            || codeBreaker[temp].equals(code[k+2])
-            || codeBreaker[temp].equals(code[k+2])){
-                System.out.println("White Pug");
-            }
-            else if(codeBreaker[temp].equals(code[k]) == false 
-            && codeBreaker[temp].equals(code[k + 1]) == false
-            && codeBreaker[temp].equals(code[k + 2]) == false
-            && codeBreaker[temp].equals(code[k + 3]) == false){
-                System.out.println("No Pugs");
-            } 
-        }
-
-
-
-        //Print based on correct or incorrect code
-        if(verifyAnswer == true){
-            System.out.println("\nThat is the correct code!");
-        }
-        else{
-            System.out.println("\nThat is not the correct code.");
-        }
-            
+            numberOfTries++;
+        } while(verifyAnswer == false && numberOfTries != 10);
     }
 
 
